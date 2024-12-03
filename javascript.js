5f2bc9ef1fc86450f6585605648012529748fc6a
blankandreas <ablank87@gmailcom>
2024-12-03 15:25:27 -0300

    add the function random color in the js add the button random color in the html

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
  const container = document.querySelector("#container")
  const square = document.querySelector("#square")
  const reset = document.querySelector("#reset")
- 
+ const color = document.querySelector("#color")
  
  reset.addEventListener("click", function() {
+  inicial()
   const allGrids = document.querySelectorAll(".sketchpad");
   allGrids.forEach(gridToRemove => {
     gridToRemove.remove();
@@ -20,7 +21,7 @@
  function choice(){
  
     a = prompt("Escolha um valor de 0 a 100")
-    
+
 
     for (let i = 0; i < a*a; i++) {   
        let size = document.createElement("div")
@@ -33,22 +34,44 @@
  }
  choice()
 
-/*  function grid(){
- let a = choice();
- for (let i = 0; i < a*a; i++) {
-
-    let size = document.createElement("div")
-    size.classList.add("sketchpad")
-    square.appendChild(size)
-    size.style.height = 500/a + "px";
-    size.style.width = 500/a + "px";
- }
-}
-grid() */
-
+function inicial (){
 square.addEventListener("mouseover", function(e) {
   const hoverGrid = e.target;
   if(hoverGrid.classList.contains("sketchpad")) {
       hoverGrid.style.backgroundColor = ("black");
   }
 })
+}
+inicial() 
+
+
+color.addEventListener("click", ()=>colorindo())
+
+function colorindo(){
+ square.addEventListener("mouseover", function(e) {
+    const hoverGrid = e.target;
+    if(hoverGrid.classList.contains("sketchpad")) {
+       
+        hoverGrid.style.backgroundColor = randomColor();
+    }
+
+  })
+}
+
+function randomColor () {
+let r =  Math.floor(Math.random()*255);
+let g =  Math.floor(Math.random()*255);
+let b =  Math.floor(Math.random()*255);
+let a =  Math.random()*1
+
+let colorFinal = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+
+console.log(colorFinal)
+return colorFinal
+
+   }
+
+
+ 
+
+   
