907ee9664ac24804c1efc4d01d33c27b0f232caf
blankandreas <ablank87@gmailcom>
2024-12-03 11:01:27 -0300

    Changes in html, css and js
    html add the reset button
    css add flex display  to the button and a border to the square
    js create the code for the button and the changes in the loop formula

--- javascript.js
+++ javascript.js
@@ -1,16 +1,54 @@
  const container = document.querySelector("#container")
  const square = document.querySelector("#square")
+ const reset = document.querySelector("#reset")
+ 
+ 
+ reset.addEventListener("click", function() {
+  const allGrids = document.querySelectorAll(".sketchpad");
+  allGrids.forEach(gridToRemove => {
+    gridToRemove.remove();
+});
+  })
 
 
- function grid(){
+
+
+ reset.addEventListener("click", ()=>choice())
+
+ 
+
+ function choice(){
+ 
+    a = prompt("Escolha um valor de 0 a 100")
+    
+
+    for (let i = 0; i < a*a; i++) {   
+       let size = document.createElement("div")
+       size.classList.add("sketchpad")
+       square.appendChild(size)
+       size.style.height = 500/a + "px";
+       size.style.width = 500/a + "px";
+    }
+  
+ }
+ choice()
+
+/*  function grid(){
+ let a = choice();
+ for (let i = 0; i < a*a; i++) {
+
     let size = document.createElement("div")
     size.classList.add("sketchpad")
-  /*   let newContent = document.createTextNode("Hi there and greetings!")
-    size.appendChild(newContent); */
     square.appendChild(size)
-
+    size.style.height = 500/a + "px";
+    size.style.width = 500/a + "px";
  }
-
- for (let i = 0; i < 100; i++) {
-  grid()
 }
+grid() */
+
+square.addEventListener("mouseover", function(e) {
+  const hoverGrid = e.target;
+  if(hoverGrid.classList.contains("sketchpad")) {
+      hoverGrid.style.backgroundColor = ("black");
+  }
+})
